20f1fb0ff1b2fec69a7b16013da1b6728a9061dd
sirazhudin@mail.ru <sirazhudin@mail.ru>
2022-12-18 12:33:25 -0500

    Module24_28

--- src/main/java/ru/skillfactory/Ex28/io/XlsWriter.java
+++ src/main/java/ru/skillfactory/Ex28/io/XlsWriter.java
@@ -1,7 +1,6 @@
 package ru.skillfactory.Ex28.io;
 
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import ru.skillfactory.Ex28.model.Statistics;
@@ -12,6 +11,8 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+
+
 public class XlsWriter {
     private static final Logger logger = Logger.getLogger(XlsWriter.class.getName());
     private XlsWriter(){}
@@ -20,23 +21,37 @@ public class XlsWriter {
         logger.log(Level.INFO,"Writing to " + pathToWrt + " file - started!");
         XSSFWorkbook statisticsXxssfWorkbook = new XSSFWorkbook();
         XSSFSheet statisticsXssfSheet = statisticsXxssfWorkbook.createSheet("Statistics");
+//setting up fonts
 
+        Font headerFont = statisticsXxssfWorkbook.createFont();
+        headerFont.setBold(true);
+        headerFont.setFontHeightInPoints((short) 14);
+        CellStyle headerStyle = statisticsXxssfWorkbook.createCellStyle();
+        headerStyle.setFont(headerFont);
+       // headerStyle.setFillBackgroundColor( );
+// end of setting up fonts
         int rowNum = 0;
         Row headerRow = statisticsXssfSheet.createRow(rowNum++);
         Cell profileHeaderCell = headerRow.createCell(0);
         profileHeaderCell.setCellValue("Study Profile");
+        profileHeaderCell.setCellStyle(headerStyle);
 
         Cell averageSocoreHeaderCell = headerRow.createCell(1);
         averageSocoreHeaderCell.setCellValue("Average ScoreMark");
+        averageSocoreHeaderCell.setCellStyle(headerStyle);
 
         Cell numberOfStudentsHeaderCell = headerRow.createCell(2);
         numberOfStudentsHeaderCell.setCellValue("Number of Students");
+        numberOfStudentsHeaderCell.setCellStyle(headerStyle);
 
         Cell numberOfUniverisitiesHeaderCell = headerRow.createCell(3);
         numberOfUniverisitiesHeaderCell.setCellValue("Number of Universities");
+        numberOfUniverisitiesHeaderCell.setCellStyle(headerStyle);
 
         Cell universityHeaderCell = headerRow.createCell(4);
         universityHeaderCell.setCellValue("University");
+        //set header style
+        universityHeaderCell.setCellStyle(headerStyle);
 
         for (Statistics statistics:statisticsList) {
              Row rowData = statisticsXssfSheet.createRow(rowNum++);
@@ -53,6 +68,9 @@ public class XlsWriter {
         }
         try (FileOutputStream fileOutputStream = new FileOutputStream(pathToWrt)) {
             statisticsXxssfWorkbook.write(fileOutputStream);
-        }
+        } catch(IOException e) {
+            logger.log(Level.SEVERE, "Writing to   " + pathToWrt + " file failed ",e);
+            return;
+        }logger.log(Level.INFO,"Write to " + pathToWrt + " was successful");
     }
 }
