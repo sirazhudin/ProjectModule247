aea978301231b09f224b7908b8894d7e8d098f33
sirazhudin@mail.ru <sirazhudin@mail.ru>
2022-12-25 21:14:12 -0500

    Module24_29 logger added

--- src/main/java/ru/skillfactory/Ex29/MainOld.java
+++ src/main/java/ru/skillfactory/Ex29/MainOld.java
@@ -37,17 +37,47 @@ public class MainOld {
                 XlsReader.readXlsUniversities("src/main/resources/universityInfo.xlsx");
         UniversityComparator universityComparator =
                 UtilComparator.getUniversityComparatorTypeClass(UniversityTypeComparatorEnum.YEAR_OF_FOUNDATION);
-
+//        universities.stream().sorted(universityComparator).forEach(System.out::println);
+//        System.out.println("To-Json---------------------------------------To-Json");
 
         universities.sort(universityComparator);
-
+//        String universitiesToJson = UtilJson.universityListToJson(universities);
+//        System.out.println(universitiesToJson);
+//
+//        System.out.println("End_To-Json------------From-Json--------------End_To-Json");
+//
+//        List<University> universitiesFromJson = UtilJson.jsonToUniversityList(universitiesToJson);
+//        System.out.println(universities.size()==universitiesFromJson.size());
+//
+//        System.out.println("\n__________From-Json_End________________________\n");
+//        universities.forEach(university -> {String universityToJson = UtilJson.universityToJson(university);
+//            System.out.println("\nTo_Json_Test_Each----------------To-Json-Test_Each\n");
+//            System.out.println(universityToJson);
+//        University universityFromJson = UtilJson.jsonToUniversity(universityToJson);
+//            System.out.println("\n------To_University_from_Json----------------\n");
+//            System.out.println(universityFromJson);});
+//        System.out.println("\n--------To_University_from_Json----------------\n");
+        // ----- students
+        // ----- students
         List<Student> students =
                 XlsReader.readXlsStudents("src/main/resources/universityInfo.xlsx");
         StudentComparator studentComparator =
                 UtilComparator.getStudentComparatorTypeClass(StudentTypeComparatorEnum.AVG_EXAM_SCORE);
 //        students.stream().sorted(studentComparator).forEach(System.out::println);
         students.sort(studentComparator);
-
+//        String studentsToJson = UtilJson.studentListToJson(students);
+//        System.out.println(studentsToJson);
+//        //stdlist
+//        List<Student> studentsFromJson = UtilJson.jsonToStudentList(studentsToJson);
+//        System.out.println("\nTest reversing to fromJson is TRUE------ \n");
+//        System.out.println(students.size() == studentsFromJson.size());
+//        System.out.println("\nTest reversing to fromJson ------ \n");
+//        students.forEach(student-> {
+//            String studentToJsonStr = UtilJson.studentToJson(student);
+//            System.out.println(studentToJsonStr);
+//            Student studentFromJsonStud = UtilJson.jsonToStudent(studentToJsonStr);
+//            System.out.println(studentFromJsonStud);
+//        });
 
         //writing
         List<Statistics> statisticsList = UtilStatistics.createStatistics(students,universities);
@@ -59,8 +89,6 @@ public class MainOld {
         completeInfo.setTimeStamp(new Date());
 
         XmlWriter.completeInfoToXml(completeInfo);
-        System.out.println("To-Json---------------------------------------To-Json");
-
 
         logger.log(Level.INFO, "Main module finished!");
     }
