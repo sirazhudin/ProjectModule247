14617efaff71b3b8cdc12d10935c897af155170b
sirazhudin@mail.ru <sirazhudin@mail.ru>
2022-12-25 20:16:13 -0500

    Module24_29

---
+++ src/main/java/ru/skillfactory/Ex29/model/CompleteInfo.java
@@ -0,0 +1,63 @@
+package ru.skillfactory.Ex29.model;
+import jakarta.xml.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+
+@XmlRootElement(name="root")//will be <root></root>
+@XmlAccessorType(XmlAccessType.FIELD)
+public class CompleteInfo {
+    //for Student class List
+    @XmlElementWrapper(name = "studentsInfo")
+    @XmlElement(name="studentEntry")
+    private List<Student> studentList;
+
+    @XmlElementWrapper(name = "universitiesInfo")
+    @XmlElement(name="universityEntry")
+    private List<University> universityList;
+
+    @XmlElementWrapper(name = "statisticsInfo")
+    @XmlElement(name = "statisticsEntry")
+    private List<Statistics> statisticsList;
+
+    @XmlElement(name = "processedAt")
+    private Date timeStamp;//bottom
+
+    public List<Student> getStudentList() {
+        return studentList;
+    }
+
+    public void setStudentList(List<Student> studentList) {
+        this.studentList = studentList;
+    }
+
+    public List<University> getUniversityList() {
+        return universityList;
+    }
+
+    public void setUniversityList(List<University> universityList) {
+        this.universityList = universityList;
+    }
+
+    public void setStatisticsList(List<Statistics> statisticsList) {
+        this.statisticsList = statisticsList;
+    }
+
+    public Date getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(Date timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public List<Statistics> getStatisticsList() {
+        return statisticsList;
+    }
+//
+//    public CompleteInfo setStatisticsList(List<Statistics>) {
+//        this.statisticsList=statisticsList;
+//        return this;
+//    }
+
+}
